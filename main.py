c1be36f82359832f693e5c433604fc74d4d679e5
BohdanBabii <bohdanbabii2000@gmail.com>
2026-08-13 15:58:16 +0200

    Added simple logic

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "Felix"
+moechteProgrammieren = False
+
+if name == "Jonas" and moechteProgrammieren == True:
+    print("Jonas möchte programmieren :) !!!")
+elif name != "Jonas" and moechteProgrammieren == True:
+    print("Jemand möchte programmieren!!!")
+else:
+    print("Niemand möchte Programmieren!!!")
